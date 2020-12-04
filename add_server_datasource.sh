00e9c5c031f74240113b3c3dd635174c238a57c0
Frederic Pesquet <frederic.pesquet@fr.ibm.com>
2020-12-04 10:41:31 +0100

    add oc3 support

--- add_server_datasource.sh
+++ add_server_datasource.sh
@@ -2,7 +2,7 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 set -e
 
-: ${GRAFANA_ROUTE?need GRAFANA_ROUTE set with the route to the Grafana instance that you got when installing the monitoring stack}
+: ${GRAFANA_ROUTE?need GRAFANA_ROUTE set with the route to the Grafana instance that you got when installing the monitoring stack, for example grafana-jmeter-monitoring.apps.yourserver.com}
 
 if ! oc projects 1>/dev/null; then
     echo "please login in the target cluster as admin before running this script"
@@ -10,11 +10,16 @@ if ! oc projects 1>/dev/null; then
     exit -1
 fi
 
-PROMETHEUS_SERVER=$(oc cluster-info | sed -n 's@.*https://api\.\([^:]*\).*@\1@p')
+PROMETHEUS_SERVER=$(oc cluster-info | sed -n 's@.*https://\([^:]*\).*@\1@p')
+PROMETHEUS_SERVER=${PROMETHEUS_SERVER/api./} # OC4
 PROMETHEUS_SERVER_SIMPLE_NAME=${PROMETHEUS_SERVER/.*/}
 echo "Create Server datasource for the current server '$PROMETHEUS_SERVER' in grafana instance running in '$GRAFANA_ROUTE'"
 PROMETHEUS_SERVER_URL="https://prometheus-k8s-openshift-monitoring.apps.$PROMETHEUS_SERVER"
 PROMETHEUS_SERVER_TOKEN=$(oc sa get-token prometheus-k8s -n openshift-monitoring)
+if [[ ("$PROMETHEUS_SERVER_TOKEN" == "") || ("$PROMETHEUS_SERVER" == "") ]]; then
+    echo "Something is wrong, I have: PROMETHEUS_SERVER='$PROMETHEUS_SERVER' ; PROMETHEUS_SERVER_TOKEN='$PROMETHEUS_SERVER_TOKEN' which does not seem right. Sorry :-("
+    exit -1
+fi
 sed -e "s@\${PROMETHEUS_SERVER_SIMPLE_NAME}@${PROMETHEUS_SERVER_SIMPLE_NAME}@" \
     -e "s@\${PROMETHEUS_SERVER_URL}@${PROMETHEUS_SERVER_URL}@" \
     -e "s@\${PROMETHEUS_SERVER_TOKEN}@${PROMETHEUS_SERVER_TOKEN}@" \
